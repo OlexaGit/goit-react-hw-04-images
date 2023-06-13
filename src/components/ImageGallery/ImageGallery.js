79ab6a686a317d2f34aa031f6b88c1fd9756fbe7
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-06-13 18:43:18 +0300

    update imageGallery

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,16 +1,12 @@
-import React, { Component } from 'react';
 import ImageGalleryItem from '../ImageGalleryItem/ImageGalleryItem';
 import css from './ImageGallery.module.css';
 
-class ImageGallery extends Component {
-  render() {
-    const { onGallery } = this.props;
-    return (
-      <ul className={css.ImageGallery}>
-        <ImageGalleryItem onGallery={onGallery} />
-      </ul>
-    );
-  }
-}
+const ImageGallery = ({ onGallery }) => {
+  return (
+    <ul className={css.ImageGallery}>
+      <ImageGalleryItem onGallery={onGallery} />
+    </ul>
+  );
+};
 
 export default ImageGallery;
