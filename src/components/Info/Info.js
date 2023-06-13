377fb125443a9f697bee233b313bf46352e86c63
OlexaGit <109541565+OlexaGit@users.noreply.github.com>
2023-06-13 18:17:16 +0300

    update Api, Button and Info

--- src/components/Info/Info.js
+++ src/components/Info/Info.js
@@ -1,9 +1,6 @@
-import React, { Component } from 'react';
 import css from './Info.module.css';
 
-class Info extends Component {
-  render() {
-    return <h1 className={css.info}> Nothing found! try again...</h1>;
-  }
-}
+const Info = () => {
+  return <h1 className={css.info}> Nothing found! try again...</h1>;
+};
 export default Info;
